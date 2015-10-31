00a2bf0ab148faa626ff58d07a79ceb2809e4311
sendhilkumarg <sendhilkumarg@gmail.com>
2015-10-31 13:20:13 +1100

    first cut

---
+++ RatingControl.swift
@@ -0,0 +1,102 @@
+//
+//  RatingControl.swift
+//  spa
+//
+//  Created by Sendhil kumar Gurunathan on 10/31/15.
+//  Copyright © 2015 Sendhil kumar Gurunathan. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    var rating = 0 {
+        didSet{
+            setNeedsLayout()
+        }
+    }
+    
+    var stars = 5
+        {
+        didSet{
+            setNeedsLayout()
+        }
+    }
+
+    
+    var ratingButtons = [ UIButton]()
+    
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+     required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let emptyStarImage = UIImage(named: "emptyStar")
+        let filledStarmage = UIImage(named: "filledStar")
+        
+        for _ in 0..<stars {
+
+            
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.setImage(emptyStarImage, forState: .Normal)
+        button.setImage(filledStarmage, forState: .Selected)
+        button.setImage(filledStarmage, forState: [.Highlighted , .Selected ])
+            button.adjustsImageWhenHighlighted = false;
+            
+        //button.backgroundColor = UIColor.blueColor()
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+        addSubview(button)
+            
+        ratingButtons.append(button)
+
+        //ratingButtons += [button]
+        }
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 250, height: 44)
+    }
+    
+    override func layoutSubviews() {
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+            button.frame = buttonFrame
+        }
+        updateButtonSelectionStates()
+        
+    }
+    // MARK: Button Action
+    
+    func ratingButtonTapped(button:UIButton){
+        
+        rating = ratingButtons.indexOf(button)! + 1;
+       // updateButtonSelectionStates();
+
+        
+        //print("button tapped")
+        /*
+        let alert = UIAlertController(title: "Hello!", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
+        let alertAction = UIAlertAction(title: "OK!", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in }
+        alert.addAction(alertAction)
+       // presentViewController(alert, animated: true) { () -> Void in }
+        
+        */
+        
+        
+    }
+    
+    func updateButtonSelectionStates(){
+        for (index,button1) in ratingButtons.enumerate(){
+            button1.selected = index < rating;
+        }
+    }
+    
+}
---
+++ spa/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  spa
+//
+//  Created by Sendhil kumar Gurunathan on 10/24/15.
+//  Copyright © 2015 Sendhil kumar Gurunathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UITextFieldDelegate ,
+UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+
+    //MARK: Properties
+    
+    //@IBOutlet weak var lblText: UILabel!
+    @IBOutlet weak var lblText1: UILabel!
+
+    @IBOutlet weak var btn1: UIButton!
+   
+    @IBOutlet weak var btn2: UIButton!
+//    @IBOutlet weak var btnClicker: UIButton!
+    
+    @IBOutlet weak var txtSearch: UITextField!
+
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        //lblText1.text = "please select a image";
+        txtSearch.resignFirstResponder()
+        let imagePickerController =  UIImagePickerController()
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePickerController.delegate = self
+         presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    override func viewDidLayoutSubviews() {
+        ratingControl.stars=6;
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        txtSearch.delegate = self
+        //ratingControl.stars=6
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+/*
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    */
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        lblText1.text = textField.text;
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage;
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //MARK: UINavigationControllerDelegate
+    
+    //MARK: Actions
+   
+
+    
+    @IBAction func btnClick(sender: UIButton) {
+        // lblText1.text = "Hi Sendhil!";
+        btn1.enabled = false;
+        btn2.enabled = true;
+        
+        lblText1.text = "Hi \(ratingControl.stars)"
+    }
+    
+    @IBAction func btnClickAgain(sender: UIButton) {
+        lblText1.text = "Hi Again!";
+        btn1.enabled = true;
+        btn2.enabled = false;
+
+    }
+}
+
